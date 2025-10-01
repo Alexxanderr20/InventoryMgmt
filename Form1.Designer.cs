c1506bd374a97789312bea3ef6c372f6f9f29c79
Alexander <alexmu419@icloud.com>
2025-10-01 00:31:55 -0700

    Added form logic and UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,19 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dgvParts;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btnAddPart;
+        private System.Windows.Forms.Button btnModifyPart;
+        private System.Windows.Forms.Button btnDeletePart;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnModifyProduct;
+        private System.Windows.Forms.Button btnDeleteProduct;
+        private System.Windows.Forms.Button btnExit;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +24,86 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dgvParts = new System.Windows.Forms.DataGridView();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnAddPart = new System.Windows.Forms.Button();
+            this.btnModifyPart = new System.Windows.Forms.Button();
+            this.btnDeletePart = new System.Windows.Forms.Button();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnModifyProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
 
-        #endregion
+            //Title
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Text = "Iventory Management App";
+
+            //Parts
+            this.dgvParts.Location = new System.Drawing.Point(20, 80);
+            this.dgvParts.Size = new System.Drawing.Size(500, 200);
+
+            //Products
+            this.dgvProducts.Location = new System.Drawing.Point(540, 80);
+            this.dgvProducts.Size = new System.Drawing.Size(500, 20);
+
+            //Part Buttons
+            this.btnAddPart.Text = "Add";
+            this.btnAddPart.Location = new System.Drawing.Point(20, 300);
+            this.btnAddPart.Click += new System.EventHandler(this.btnAddPart_Click);
+
+            this.btnModifyPart.Text = "Modify";
+            this.btnModifyPart.Location = new System.Drawing.Point(100, 300);
+            this.btnModifyPart.Click += new System.EventHandler(this.btnModifyPart_Click);
+
+            this.btnDeletePart.Text = "Delete";
+            this.btnDeletePart.Location = new System.Drawing.Point(180, 300);
+            this.btnDeletePart.Click += new System.EventHandler(this.btnDeletePart_Click);
+
+            //Product Buttons
+            this.btnAddProduct.Text = "Add";
+            this.btnAddProduct.Location = new System.Drawing.Point(540, 300);
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
+
+            this.btnModifyProduct.Text = "Modify";
+            this.btnModifyPart.Location = new System.Drawing.Point(620, 300);
+            this.btnModifyPart.Click += new System.EventHandler(this.btnModifyPart_Click);
+
+            this.btnDeleteProduct.Text = "Delete";
+            this.btnDeleteProduct.Location = new System.Drawing.Point(700, 300);
+            this.btnDeleteProduct.Click += new System.EventHandler(this.btnDeleteProduct_Click);
+
+            //Exit button
+            this.btnExit.Text = "Exit";
+            this.btnExit.Location = new System.Drawing.Point(940, 340);
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+
+            //Form 1
+            this.ClientSize = new System.Drawing.Size(1080, 400);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvParts);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnAddPart);
+            this.Controls.Add(this.btnModifyPart);
+            this.Controls.Add(this.btnDeletePart);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.btnModifyProduct);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.btnExit);
+            this.Text = "Inventory Management System";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InventoryMgmt
@@ -15,6 +8,40 @@ namespace InventoryMgmt
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAddPart_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Add Part clicked (placeholder)");
+        }
+
+        private void btnModifyPart_Click(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Modify Part clicked (placeholder)");
+        }
+
+        private void btnDeletePart_Click(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Delete Part clicked (placeholder)");
+        }
+        private void btnAddProduct_Click(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Add Product clicked (placeholder)");
+        }
+
+        private void btnModifyProduct_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Modify Product clicked (placeholder)");
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Delete Product clicked (placeholder) ");
         }
     }
 }
